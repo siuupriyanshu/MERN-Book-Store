fb42b65ab156fa5015da67d52229872feb58fd96
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-08-01 21:21:48 +0545

    updated

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,12 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
+
 //CORS policy
  app.use(cors());
 
